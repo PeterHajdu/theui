09e8c9066984bcac4d1f766bebd7351e5599dddc
Peter Hajdu <EDIT>
2015-03-10 12:59:38 +0100

    implement list restructure with original heights

--- include/theui/list_restructure.hpp
+++ include/theui/list_restructure.hpp
@@ -9,22 +9,70 @@ namespace ui
 {
 class Window;
 
+class WindowResizer
+{
+  public:
+    inline
+    WindowResizer( const Window& parent )
+      : m_parent( parent )
+      , m_parent_x( parent.top_left().x )
+      , m_parent_width( parent.size().width )
+    {
+    }
+
+
+    inline void
+    resize_to_equal_heights() const
+    {
+      const int child_height( m_parent.size().height / m_parent.children().size() );
+      int next_y( m_parent.top_left().y );
+
+      for ( const auto& child : m_parent.children() )
+      {
+        resize_with_y_and_height( next_y, child_height, *child );
+        next_y += child_height;
+      }
+    }
+
+
+    inline void
+    resize_with_original_heights() const
+    {
+      int next_y( m_parent.top_left().y );
+
+      for ( const auto& child : m_parent.children() )
+      {
+        const int original_height( child->size().height );
+        resize_with_y_and_height( next_y, original_height, *child );
+        next_y += original_height;
+      }
+    }
+
+
+  private:
+    inline void
+    resize_with_y_and_height( int y, int height, Window& child ) const
+    {
+      child.move_and_resize(
+          { m_parent_x, y },
+          { m_parent_width, height } );
+    }
+
+    const Window& m_parent;
+    const int m_parent_x;
+    const int m_parent_width;
+};
+
 inline void
 list_window_restructure( const Window& parent )
 {
-  const auto& x( parent.top_left().x );
-  const auto& width( parent.size().width );
-  const int child_height( parent.size().height / parent.children().size() );
+  WindowResizer( parent ).resize_to_equal_heights();
+}
 
-  int next_y( parent.top_left().y );
-
-  for ( const auto& child : parent.children() )
-  {
-    child->move_and_resize(
-        { x, next_y },
-        { width, child_height } );
-    next_y += child_height;
-  }
+inline void
+list_window_restructure_with_fixed_height_from_top( const Window& parent )
+{
+  WindowResizer( parent ).resize_with_original_heights();
 }
 
 }
--- test/test_window_restructure.cpp
+++ test/test_window_restructure.cpp
@@ -22,6 +22,10 @@ class WindowRestructureBundle
       , parent( *m_parent )
     {
       add_children( number_of_children );
+    }
+
+    void restructure()
+    {
       m_window_restructure( parent );
     }
 
@@ -34,12 +38,16 @@ class WindowRestructureBundle
           Equals( true ) );
     }
 
+    const the::ui::Window::Coordinate original_child_top_left{ 34, 87 };
+    const the::ui::Size original_child_size{ 234, 987 };
   private:
     void add_children( size_t n )
     {
       for ( size_t i( 0 ); i < n; ++i )
       {
-        m_parent->add_child( std::make_unique< the::ui::Window >() );
+        m_parent->add_child( std::make_unique< the::ui::Window >(
+              original_child_top_left,
+              original_child_size ) );
       }
     }
 
@@ -62,6 +70,7 @@ Describe( a_list_window_restructure )
         size,
         the::ui::list_window_restructure,
         number_of_children );
+    restructure_bundle->restructure();
   }
 
   const the::ui::Window::Container& children()
@@ -78,7 +87,7 @@ Describe( a_list_window_restructure )
           } );
   }
 
-  It( sets_the_size_x_to_the_parents )
+  It( sets_the_width_to_the_parents )
   {
     restructure_bundle->assert_each_child(
           [ width = size.width ]( const auto& child ) -> bool
@@ -114,4 +123,66 @@ Describe( a_list_window_restructure )
   const the::ui::Size size{ 1000, 2000 };
 };
 
+Describe( a_list_window_restructure_with_fixed_height_from_top )
+{
+
+  void SetUp()
+  {
+    restructure_bundle = std::make_unique< test::WindowRestructureBundle >(
+        top_left,
+        size,
+        the::ui::list_window_restructure_with_fixed_height_from_top,
+        number_of_children );
+    restructure_bundle->restructure();
+  }
+
+  const the::ui::Window::Container& children()
+  {
+    return restructure_bundle->parent.children();
+  }
+
+  It( sets_the_top_left_x_coordinate_to_the_parents )
+  {
+    restructure_bundle->assert_each_child(
+          [ x = top_left.x ]( const auto& child ) -> bool
+          {
+            return x == child->top_left().x;
+          } );
+  }
+
+  It( sets_the_width_to_the_parents )
+  {
+    restructure_bundle->assert_each_child(
+          [ width = size.width ]( const auto& child ) -> bool
+          {
+            return child->size().width == width;
+          } );
+  }
+
+  It( does_not_touch_original_height )
+  {
+    restructure_bundle->assert_each_child(
+          [ original_height = restructure_bundle->original_child_size.height ]( const auto& child ) -> bool
+          {
+            return child->size().height == original_height;
+          } );
+  }
+
+  It( increments_y_coordinates_with_the_original_height_of_the_previous_children )
+  {
+    int next_y( restructure_bundle->parent.top_left().y );
+    const auto child_height( restructure_bundle->original_child_size.height );
+
+    for ( const auto& child : children() )
+    {
+      AssertThat( child->top_left().y, Equals( next_y ) );
+      next_y += child_height;
+    }
+  }
+
+  const size_t number_of_children{ 5u };
+  std::unique_ptr< test::WindowRestructureBundle > restructure_bundle;
+  const the::ui::Window::Coordinate top_left{ 100, 200 };
+  const the::ui::Size size{ 1000, 2000 };
+};
 
